d448e0a842d6d1af7823be8b981e06e8e0ff6ebf
lucopdev <lucopdev@gmail.com>
2022-12-23 15:33:55 -0300

    add H1

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const createDiv = (id) => {
   div.id = id;
   document.body.appendChild(div);
 };
+
+const createH1 = (id) => {
+  const div = document.getElementById('input-div');
+  const h1 = document.createElement('h1');
+  h1.id = id;
+  h1.innerText = 'Gerador de memes';
+  div.appendChild(h1);
+};
